18ddcd6574e1e0c7e364e969341e2cbd2e4c207b
alexander-hm <alexanderhuang5@gmail.com>
2023-08-29 02:23:06 +0200

    Update database enhancement resources

--- Resources/Data_Visualization.py
+++ Resources/Data_Visualization.py
@@ -17,14 +17,23 @@ color = sns.color_palette()
 from pandas.plotting import table
 from datetime import datetime
 
+# Queries
+Query1 = "SELECT fund_symbol, fund_long_name, ROUND((fund_return_2020 + fund_return_2019 + fund_return_2018 + fund_return_2017 + fund_return_2016 + fund_return_2015 + fund_return_2014 + fund_return_2013 + fund_return_2012 + fund_return_2011 + fund_return_2010 + fund_return_2009 + fund_return_2008 + fund_return_2007 + fund_return_2006 + fund_return_2005 + fund_return_2004 + fund_return_2003 + fund_return_2002 + fund_return_2001 + fund_return_2000) / 21, 4) AS avg_yearly_return FROM returns r INNER JOIN general g ON r.fund_id = g.fund_id ORDER BY avg_yearly_return DESC LIMIT 10;"
+Query2 = "SELECT fund_symbol, ROUND(AVG(volume), 0) average_volume FROM daily_performance dp INNER JOIN general g ON dp.fund_id = g.fund_id GROUP BY fund_symbol ORDER BY average_volume DESC LIMIT 10;"
+Query3 = "SELECT fund_symbol, price_date, ROUND((close - open)/open, 4) AS daily_change FROM (SELECT daily_performance.*, MAX(ROUND((close - open)/open, 4)) OVER(PARTITION BY fund_id) as max_change from daily_performance) AS dp INNER JOIN general g ON dp.fund_id = g.fund_id WHERE ROUND((close - open)/open, 4) = max_change ORDER BY daily_change DESC LIMIT 10;"
+Query4 = "SELECT fund_category, ROUND(AVG((fund_return_2020 + fund_return_2019 + fund_return_2018 + fund_return_2017 + fund_return_2016 + fund_return_2015 + fund_return_2014 + fund_return_2013 + fund_return_2012 + fund_return_2011 + fund_return_2010 + fund_return_2009 + fund_return_2008 + fund_return_2007 + fund_return_2006 + fund_return_2005 + fund_return_2004 + fund_return_2003 + fund_return_2002 + fund_return_2001 + fund_return_2000) / 21), 4) AS category_avg_yearly_return FROM returns r INNER JOIN general g ON r.fund_id = g.fund_id INNER JOIN category c ON g.category_id = c.category_id GROUP BY fund_category ORDER BY category_avg_yearly_return DESC LIMIT 10;"
+
+Query1NL = "SELECT fund_symbol, fund_long_name, ROUND((fund_return_2020 + fund_return_2019 + fund_return_2018 + fund_return_2017 + fund_return_2016 + fund_return_2015 + fund_return_2014 + fund_return_2013 + fund_return_2012 + fund_return_2011 + fund_return_2010 + fund_return_2009 + fund_return_2008 + fund_return_2007 + fund_return_2006 + fund_return_2005 + fund_return_2004 + fund_return_2003 + fund_return_2002 + fund_return_2001 + fund_return_2000) / 21, 4) AS avg_yearly_return FROM returns r INNER JOIN general g ON r.fund_id = g.fund_id ORDER BY avg_yearly_return DESC;"
+Query2NL = "SELECT fund_symbol, ROUND(AVG(volume), 0) average_volume FROM daily_performance dp INNER JOIN general g ON dp.fund_id = g.fund_id GROUP BY fund_symbol ORDER BY average_volume DESC;"
+
 # Connect to MySQL server
-mydb=mysql.connector.connect(host='localhost',user='root',password='LillyNoelle1!',database='us_funds')
+mydb=mysql.connector.connect(host='localhost',user='root',password='********',database='us_funds')
 mycursor=mydb.cursor()
 
 
 # ----- Research Question 1 -----
 # Selects columns 'fund_symbol', 'fund_long_name', and avg_yearly_return
-mycursor.execute("SELECT fund_symbol, fund_long_name, ROUND((fund_return_2020 + fund_return_2019 + fund_return_2018 + fund_return_2017 + fund_return_2016 + fund_return_2015 + fund_return_2014 + fund_return_2013 + fund_return_2012 + fund_return_2011 + fund_return_2010 + fund_return_2009 + fund_return_2008 + fund_return_2007 + fund_return_2006 + fund_return_2005 + fund_return_2004 + fund_return_2003 + fund_return_2002 + fund_return_2001 + fund_return_2000) / 21, 4) AS avg_yearly_return FROM returns r INNER JOIN general g ON r.fund_id = g.fund_id ORDER BY avg_yearly_return DESC LIMIT 10;")
+mycursor.execute(Query1)
 result = mycursor.fetchall
 
 # save query results into arrays
@@ -49,7 +58,7 @@ plt.show()
 
 # ----- Research Question 2 -----
 # Selects columns 'fund_symbol' and 'average_volume'
-mycursor.execute("SELECT fund_symbol, ROUND(AVG(volume), 0) average_volume FROM daily_performance dp INNER JOIN general g ON dp.fund_id = g.fund_id GROUP BY fund_symbol ORDER BY average_volume DESC LIMIT 10;")
+mycursor.execute(Query2)
 result = mycursor.fetchall
 
 fund_symbol = []
@@ -72,7 +81,7 @@ plt.show()
 
 # ----- Research Question 3 -----
 # Selects columns 'fund_symbol', 'price_date', and 'daily_change'
-mycursor.execute("SELECT fund_symbol, price_date, ROUND((close - open)/open, 4) AS daily_change FROM (SELECT daily_performance.*, MAX(ROUND((close - open)/open, 4)) OVER(PARTITION BY fund_id) as max_change from daily_performance) AS dp INNER JOIN general g ON dp.fund_id = g.fund_id WHERE ROUND((close - open)/open, 4) = max_change ORDER BY daily_change DESC LIMIT 10;")
+mycursor.execute(Query3)
 result = mycursor.fetchall
 
 fund_symbol = []
@@ -105,7 +114,7 @@ plt.show()
 
 # ----- Research Question 4 -----
 # Selects columns 'fund_category' and 'category_avg_yearly_return'
-mycursor.execute("SELECT fund_category, ROUND(AVG((fund_return_2020 + fund_return_2019 + fund_return_2018 + fund_return_2017 + fund_return_2016 + fund_return_2015 + fund_return_2014 + fund_return_2013 + fund_return_2012 + fund_return_2011 + fund_return_2010 + fund_return_2009 + fund_return_2008 + fund_return_2007 + fund_return_2006 + fund_return_2005 + fund_return_2004 + fund_return_2003 + fund_return_2002 + fund_return_2001 + fund_return_2000) / 21), 4) AS category_avg_yearly_return FROM returns r INNER JOIN general g ON r.fund_id = g.fund_id INNER JOIN category c ON g.category_id = c.category_id GROUP BY fund_category ORDER BY category_avg_yearly_return DESC LIMIT 10;")
+mycursor.execute(Query4)
 result = mycursor.fetchall
 
 fund_category = []
@@ -126,3 +135,55 @@ plt.xticks(rotation=25, horizontalalignment='right')
 plt.tight_layout()
 # display the graph
 plt.show()
+
+
+# ---------- STATISTICAL ANALYSIS ----------
+
+# Setup
+sns.set_style("whitegrid")
+color = sns.color_palette()
+
+# Connect to MySQL server
+try:
+    mydb = mysql.connector.connect(host='localhost', user='root', password='********', database='us_funds')
+    mycursor = mydb.cursor()
+
+    # Function to fetch data and return as DataFrame
+    def fetch_data(query):
+        mycursor.execute(query)
+        result = mycursor.fetchall()
+        return pd.DataFrame(result, columns=mycursor.column_names)
+
+    # Fetch average yearly return and average volume data
+    avg_yearly_return_data = fetch_data(Query1NL)  # SQL for average yearly return
+    avg_daily_volume_data = fetch_data(Query2NL)   # SQL for average daily volume
+
+    # Merge the two DataFrames on fund_symbol
+    merged_data = pd.merge(avg_yearly_return_data, avg_daily_volume_data, on="fund_symbol")
+
+    print(avg_yearly_return_data.shape)
+    print(avg_daily_volume_data.shape)
+    print(merged_data.shape)
+
+    # Convert to numeric types
+    merged_data['avg_yearly_return'] = pd.to_numeric(merged_data['avg_yearly_return'], errors='coerce')
+    merged_data['average_volume'] = pd.to_numeric(merged_data['average_volume'], errors='coerce')
+
+    # print(merged_data['avg_yearly_return'].dtype)
+    # print(merged_data['average_volume'].dtype)
+
+    # Calculate the correlation between avg_yearly_return and average_volume
+    correlation = merged_data['avg_yearly_return'].corr(merged_data['average_volume'])
+    print(f"Correlation between average yearly return and average volume: {correlation:.2f}")
+
+    # Plotting a scatter plot to visually see the correlation
+    plt.figure(figsize=(10, 6))
+    sns.scatterplot(data=merged_data, x='avg_yearly_return', y='average_volume')
+    plt.title("Scatter plot of Average Yearly Return vs Average Volume")
+    plt.show()
+finally:
+    # Close the connection
+    if mycursor:
+        mycursor.close()
+    if mydb:
+        mydb.close()
--- Resources/Research_Analysis.sql
+++ Resources/Research_Analysis.sql
@@ -25,13 +25,13 @@ INNER JOIN general g ON dp.fund_id = g.fund_id
 WHERE ROUND((close - open)/open, 4) = max_change
 ORDER BY daily_change DESC
 LIMIT 10;
-
+#*/
 #4: What is the average yearly growth of funds? By how much are the top funds able to outperform the average?
 # Total average return across all funds
 SELECT ROUND(AVG((fund_return_2020 + fund_return_2019 + fund_return_2018 + fund_return_2017 + fund_return_2016 + fund_return_2015 + fund_return_2014 + fund_return_2013 + fund_return_2012 + fund_return_2011 + fund_return_2010 + fund_return_2009 + fund_return_2008 + fund_return_2007 + fund_return_2006 + fund_return_2005 + fund_return_2004 + fund_return_2003 + fund_return_2002 + fund_return_2001 + fund_return_2000) / 21), 4) AS total_avg_yearly_return
 FROM returns r
-INNER JOIN general g ON r.fund_id = g.fund_id
-*/
+INNER JOIN general g ON r.fund_id = g.fund_id;
+#*/
 # Average fund returns by category
 SELECT fund_category, ROUND(AVG((fund_return_2020 + fund_return_2019 + fund_return_2018 + fund_return_2017 + fund_return_2016 + fund_return_2015 + fund_return_2014 + fund_return_2013 + fund_return_2012 + fund_return_2011 + fund_return_2010 + fund_return_2009 + fund_return_2008 + fund_return_2007 + fund_return_2006 + fund_return_2005 + fund_return_2004 + fund_return_2003 + fund_return_2002 + fund_return_2001 + fund_return_2000) / 21), 4) AS category_avg_yearly_return
 FROM returns r
